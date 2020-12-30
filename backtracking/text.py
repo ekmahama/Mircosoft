5b9fbca5089bff5c56884da4f4ff430bfb1efc31
Edward <34236615+ekmahama@users.noreply.github.com>
2020-12-30 12:14:44 -0500

    Update text.py
    Add  DP version for factorial

--- backtracking/text.py
+++ backtracking/text.py
@@ -5,3 +5,9 @@ def fact(n, memo={}):
     if not n in memo:
         memo[n] = n*fact(n-1)
     return memo[n]
+
+def fact(n):
+    DP = [1 for _ in range(n+1)]
+    for i in range(1,n+1):
+        DP[i]=i*DP[i-1]
+    return DP[n]
