a6449b2bf55789f951cd7797e0befce020984479
Edward <34236615+ekmahama@users.noreply.github.com>
2020-12-30 12:07:16 -0500

    Update text.py
    for testing

--- backtracking/text.py
+++ backtracking/text.py
@@ -2,6 +2,6 @@ def fact(n, memo={}):
     if n == 0 or n == 1:
         memo[n] = 1
         return memo[n]
-    if n not in memo:
+    if not n in memo:
         memo[n] = n*fact(n-1)
     return memo[n]
